d7b9105721a3c5b3719b1cb92e2dbfb6795fafed
MarvinxKiyingi <marvin96k@gmail.com>
2022-07-31 22:31:57 +0200

    style(quizResult): add styling to the quiz result component

--- src/components/QuizResults.tsx
+++ src/components/QuizResults.tsx
@@ -1,26 +1,73 @@
-import { Button, Typography } from '@mui/material';
+import { Box, Button, styled, Typography } from '@mui/material';
+import { StyledWrapper } from '../App';
 import { useQuizContext } from '../utils/QuizContex';
 
+const StyledQuizResults = styled(Box)(({ theme }) => ({
+  padding: '0 1rem',
+  [theme.breakpoints.up('md')]: {
+    margin: 'unset',
+    display: 'grid !important',
+    gridTemplateColumns: 'repeat(10,1fr)',
+  },
+  '.quizResults-container': {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    flex: 1,
+    [theme.breakpoints.up('md')]: {
+      gridColumn: '2/-2',
+    },
+    '.quizResults-heading': {
+      fontFamily: "'Akaya Kanadaka', cursive",
+      fontWeight: 600,
+      textAlign: 'center',
+      [theme.breakpoints.up('md')]: {
+        ...theme.typography.h3,
+        fontFamily: "'Akaya Kanadaka', cursive",
+        fontWeight: 600,
+      },
+    },
+  },
+  '.quizScore-container': {
+    backgroundColor: '#FAFAFA',
+    color: '#20484F',
+    border: ' 1px solid rgba(32, 72, 79, 0.2)',
+    borderRadius: '8px',
+    height: 200,
+    display: 'flex',
+    justifyContent: 'center',
+    margin: '2rem 0 1rem',
+    '.quizScore-label': {
+      fontWeight: 700,
+      textAlign: 'center',
+      alignSelf: 'center',
+    },
+  },
+}));
 export const QuizResults = () => {
   const {
     state: { quizScore },
     handlers: { quizApiResponse, handleReset },
   } = useQuizContext();
   return (
-    <div>
-      <Typography component='h1' variant='h3'>
-        Quiz results
-      </Typography>
-      <Typography component='h2' variant='subtitle1'>
-        Quiz results
-      </Typography>
+    <StyledWrapper>
+      <StyledQuizResults component='section'>
+        <Box className='quizResults-container'>
+          <Typography className='quizResults-heading' component='h1' variant='h3'>
+            Results
+          </Typography>
 
-      <Typography component='h1' variant='h1' sx={{ fontWeight: 700 }}>
-        {quizScore} / {quizApiResponse.length}
-      </Typography>
-      <Button variant='contained' onClick={handleReset} sx={{ margin: '1rem 0rem' }}>
-        Reset quiz
-      </Button>
-    </div>
+          <Box className='quizScore-container'>
+            <Typography className='quizScore-label' component='h1' variant='h1'>
+              {quizScore} / {quizApiResponse.length}
+            </Typography>
+          </Box>
+
+          <Button variant='contained' onClick={handleReset} sx={{ margin: '1rem 0rem' }}>
+            Reset quiz
+          </Button>
+        </Box>
+      </StyledQuizResults>
+    </StyledWrapper>
   );
 };
